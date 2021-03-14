5e72653e5b1f2bb251452337c3d28e35f86e1fa3
Lucas Stefano <lk.stefano1@gmail.com>
2021-03-14 10:09:38 -0300

    Test especific from Pessoa.kt

---
+++ src/main/kotlin/digitalone/kotlin/dioeveris/testes/TestePessoa.kt
@@ -0,0 +1,9 @@
+package digitalone.kotlin.dioeveris.testes
+
+import digitalone.kotlin.dioeveris.Pessoa
+
+fun main (){
+    val Luke = Pessoa (nome = "Luke Stefano", cpf = "123.123.123-12")
+    println(Luke.nome)
+    println(Luke.cpf)
+}
